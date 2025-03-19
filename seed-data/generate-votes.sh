8f3b7afebc5b00be4765629da0f29e22224dda53
faagour <aagour2003@gmail.com>
2025-03-19 14:46:47 +0100

    all the services are working

--- seed-data/generate-votes.sh
+++ seed-data/generate-votes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # create 3000 votes (2000 for option a, 1000 for option b)
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://nginx:8001/
-ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" http://nginx:8001/
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://nginx:8001/
+ab -n 50 -c 10 -p posta -T "application/x-www-form-urlencoded" http://nginx:8001/
+ab -n 50 -c 10 -p postb -T "application/x-www-form-urlencoded" http://nginx:8001/
+ab -n 50 -c 10 -p posta -T "application/x-www-form-urlencoded" http://nginx:8001/
